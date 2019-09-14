76b9ed7093433a4c4f630b81b1a0abb87a8d7680
Lukas <niederbergerlukas@gmail.com>
2019-09-14 11:49:27 -0700

    Add static contact page

--- lib/dievergolderei_web/router.ex
+++ lib/dievergolderei_web/router.ex
@@ -17,6 +17,7 @@ defmodule DievergoldereiWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/kontakt", PageController, :contact
   end
 
   # Other scopes may use custom stacks.
