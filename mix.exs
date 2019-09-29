42df5171a5dba4a209fde19b325673700a681a14
Lukas <niederbergerlukas@gmail.com>
2019-09-28 17:49:30 -0700

    Updated version number, minimum elixir version

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Dievergolderei.MixProject do
   def project do
     [
       app: :dievergolderei,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      version: "0.2.0",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
