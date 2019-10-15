16dc060abed46f8bd585287f0aa6b38e3ea1fffe
Lukas <niederbergerlukas@gmail.com>
2019-10-15 22:11:19 +0200

    Change photos.photo_id fkey to allow deleting photos
    Prior to this change, deleting a photo that was used for a post would
    generate an error.
    
    This change modifies `posts_photo_id_fkey` to the `:nilify_all`
    behaviour, removing the post image if a photo is deleted through the
    gallery.

---
+++ priv/repo/migrations/20191015200720_update_post_photo_fkey.exs
@@ -0,0 +1,17 @@
+defmodule Dievergolderei.Repo.Migrations.UpdatePostPhotoFkey do
+  use Ecto.Migration
+
+  def up do
+    drop constraint(:posts, "posts_photo_id_fkey")
+    alter table(:posts) do
+      modify :photo_id, references(:photos, on_delete: :nilify_all)
+    end
+  end
+
+  def down do
+    drop constraint(:posts, "posts_photo_id_fkey")
+    alter table(:posts) do
+      modify :photo_id, references(:photos, on_delete: :nothing)
+    end
+  end
+end
