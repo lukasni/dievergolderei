lukasni/dievergolderei

commit 7871f25385be6e4fd09b80d069ed0f2e0256871b

Remove unused Live View code

lib/dievergolderei_web.ex
@@ -23,7 +23,6 @@ defmodule DievergoldereiWeb do
 
       import Plug.Conn
       import DievergoldereiWeb.Gettext
-      import Phoenix.LiveView.Controller, only: [live_render: 3]
       alias DievergoldereiWeb.Router.Helpers, as: Routes
     end
   end
@@ -36,7 +35,6 @@ defmodule DievergoldereiWeb do
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
-      import Phoenix.LiveView, only: [live_render: 2, live_render: 3, live_link: 1, live_link: 2]
 
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
@@ -55,7 +53,6 @@ defmodule DievergoldereiWeb do
       use Phoenix.Router
       import Plug.Conn
       import Phoenix.Controller
-      import Phoenix.LiveView.Router
     end
   end
 

lib/dievergolderei_web/router.ex
@@ -5,7 +5,6 @@ defmodule DievergoldereiWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug Phoenix.LiveView.Flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug DievergoldereiWeb.Auth
@@ -27,7 +26,6 @@ defmodule DievergoldereiWeb.Router do
     get "/", PageController, :index
     get "/kontakt", PageController, :contact
     get "/impressionen", PageController, :gallery
-    # live "/impressionen", GalleryLive
     get "/geschichte", PageController, :history
     get "/blog", PostController, :blog
     get "/blog/:month", PostController, :list

assets/css/app.scss
@@ -1,6 +1,5 @@
 /* This file is for your main application css. */
 
-@import "../../deps/phoenix_live_view/assets/css/live_view.css";
 @import "../node_modules/simple-lightbox/dist/simpleLightbox.css";
 
 @import "colors";

assets/js/app.js
@@ -16,5 +16,4 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
-import liveSocket from "./live_socket"
 import lightBox from "./gallery"

config/config.exs
@@ -19,10 +19,7 @@ config :dievergolderei, DievergoldereiWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "GbGHdYKslelLVH8Bv1STveMW3vK//AHVQt8YL4t8sooPjaFGa+vpCo5ZtRFcamXB",
   render_errors: [view: DievergoldereiWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Dievergolderei.PubSub, adapter: Phoenix.PubSub.PG2],
-  live_view: [
-    signing_salt: "WL42N5UNOYEc2v0m7pVyx8f+chokWkGw"
-  ]
+  pubsub: [name: Dievergolderei.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,

config/releases.exs
@@ -23,21 +23,11 @@ secret_key_base =
     You can generate one by calling: mix phx.gen.secret
     """
 
-lv_signing_salt =
-  System.get_env("LIVEVIEW_SIGNING_SALT") ||
-    raise """
-    environment variable LIVEVIEW_SIGNING_SALT is missing.
-    You can generate one by calling mix phx.gen.secret 32
-    """
-
 config :dievergolderei, DievergoldereiWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "80")],
   url: [host: "dievergolderei.ch", port: 443, scheme: "https"],
   secret_key_base: secret_key_base,
   server: true,
-  live_view: [
-    signing_salt: lv_signing_salt
-  ],
   force_ssl: [hsts: true],
   https: [
     :inet6,

lib/dievergolderei_web/controllers/page_controller.ex
@@ -15,11 +15,6 @@ defmodule DievergoldereiWeb.PageController do
     render(conn, "contact.html", title: "Kontakt — ", static_content: content)
   end
 
-  # def gallery(conn, _) do
-  #   conn
-  #   |> assign(:title, "Impressionen — ")
-  #   |> Phoenix.LiveView.Controller.live_render(DievergoldereiWeb.GalleryLive, session: %{})
-  # end
   def gallery(conn, _) do
     photos = Dievergolderei.Gallery.list_gallery_photos()
     render(conn, "gallery.html", title: "Impressionen — ", photos: photos)

lib/dievergolderei_web/endpoint.ex
@@ -5,8 +5,6 @@ defmodule DievergoldereiWeb.Endpoint do
     websocket: true,
     longpoll: false
 
-  socket "/live", Phoenix.LiveView.Socket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest

commit 130264235b97bd3076f4178213706b0c51969324

Fix webpack compile error
The liveview removal in 7871f25 caused a compile error because
live_view.js wasn't deleted from assets.

This change fixes the issue by deleting the file. Coverage results are
slightly improved by adding some skip comments

assets/js/live_socket.js
@@ -1,7 +1,0 @@
-import {Socket} from "phoenix"
-import LiveSocket from "phoenix_live_view"
-
-let liveSocket = new LiveSocket("/live", Socket)
-liveSocket.connect()
-
-export default liveSocket

lib/dievergolderei_web.ex
@@ -67,6 +67,8 @@ defmodule DievergoldereiWeb do
   When used, dispatch to the appropriate controller/view/etc.
   """
   defmacro __using__(which) when is_atom(which) do
+    # coveralls-ignore-start
     apply(__MODULE__, which, [])
+    # coveralls-ignore-stop
   end
 end

lib/dievergolderei_web/plugs/auth.ex
@@ -6,7 +6,9 @@ defmodule DievergoldereiWeb.Auth do
 
   alias Dievergolderei.Accounts
 
+  # coveralls-ignore-start
   def init(opts), do: opts
+  # coveralls-ignore-stop
 
   def call(conn, _opts) do
     user_id = get_session(conn, :user_id)

lib/dievergolderei_web/plugs/clacks.ex
@@ -1,7 +1,9 @@
 defmodule Clacks do
   import Plug.Conn, only: [put_resp_header: 3]
 
+  # coveralls-ignore-start
   def init(opts), do: opts
+  # coveralls-ignore-stop
 
   def call(conn, _opts) do
     conn

lib/dievergolderei_web/plugs/require_login.ex
@@ -6,7 +6,9 @@ defmodule DievergoldereiWeb.RequireLogin do
 
   alias DievergoldereiWeb.Router.Helpers, as: Routes
 
+  # coveralls-ignore-start
   def init(opts), do: opts
+  # coveralls-ignore-stop
 
   def call(conn, _opts) do
     case conn.assigns[:current_user] do

lib/dievergolderei_web/router.ex
@@ -11,10 +11,6 @@ defmodule DievergoldereiWeb.Router do
     plug Clacks
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   pipeline :admin do
     plug :put_layout, {DievergoldereiWeb.LayoutView, "admin.html"}
     plug DievergoldereiWeb.RequireLogin