e872e5f2bc5765403a11d34f0584861d23ee8320
Lukas Niederberger <niederbergerlukas@gmail.com>
2020-10-30 22:43:00 -0700

    WIP Remove Sass, add Tailwind & Postcss Add Alpine Remove old stylesheets
    This sets the project up for rebuilding the interface with tailwind

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "milligram"
-import css from "../css/app.scss"
+import css from "../css/app.css"
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -14,7 +13,6 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
-import lightBox from "./gallery"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
---
+++ assets/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [
+    "../**/*.html.eex",
+    "../**/*.html.leex",
+    "../**/views/**/*.ex",
+    "../**/live/**/*.ex",
+    "./js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  experimental: {
+    applyComplexClasses: true
+  }
+}
