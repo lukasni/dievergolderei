8238a4c2df255b16383624b0e4fb93fe60d0f05e
Lukas Niederberger <niederbergerlukas@gmail.com>
2024-03-02 17:40:40 -0800

    Update elixir version & project version number

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Dievergolderei.MixProject do
   def project do
     [
       app: :dievergolderei,
-      version: "2.1.0",
-      elixir: "~> 1.14",
+      version: "2.2.0",
+      elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
